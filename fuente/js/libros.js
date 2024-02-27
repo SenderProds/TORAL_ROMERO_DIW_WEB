738cf37c36404d656c4010464799ff7239901192
SenderProds <alextoralromero04@gmail.com>
2024-02-27 22:48:21 +0100

    Cambios

--- fuente/js/libros.js
+++ fuente/js/libros.js
@@ -3,11 +3,28 @@ let productosCarrito = 0;
 
 let poppupCarrito = document.getElementById('carrito');
 let cantidadProductos = document.getElementById('productos');
+poppupCarrito.style.cursor = "pointer";
 
-console.log(botonesCarrito);
+
+
+poppupCarrito.addEventListener("click", (e) => {
+    console.log("Carrito");
+    location.href = "../html/carrito.html";
+    
+});
 
 botonesCarrito.forEach((boton) => {
     boton.addEventListener('click', (e) => {
+        let unidades = 1;
+        if(localStorage.getItem("carrito")){
+            unidades = localStorage.getItem("carrito");
+            unidades++;
+            localStorage.setItem("carrito", unidades);
+        }else{
+
+            localStorage.setItem("carrito", unidades)
+        }
+        
         productosCarrito++;
 
         if(poppupCarrito.classList.contains('ocultar')){
--- fuente/js/sesion.js
+++ fuente/js/sesion.js
@@ -26,7 +26,27 @@ if(formularioSesion){
 function comprobarSesion() {
     if (localStorage.getItem('sesion')) {
         btnSesion.textContent = "CUENTA";
+        if(document.getElementById("contenidoLogin")){
+            document.getElementById("contenidoLogin").classList.add("ocultar");
+            document.getElementById("usuario").innerHTML = JSON.parse(localStorage.getItem("sesion")).user;
+            document.getElementById("cuenta").classList.remove("ocultar");
+            document.getElementById("cuenta").classList.add("cuenta");
+            document.getElementById("cerrarSesion").addEventListener('click', (e) => {
+                e.preventDefault();
+                localStorage.removeItem("sesion");
+                comprobarSesion();
+            });
+
+        }
     } else {
+        if(document.getElementById("contenidoLogin").classList.has("ocultar")){
+            document.getElementById("cuenta").classList.add("ocultar");
+            
+        }else{
+            document.getElementById("cuenta").classList.remove("ocultar");
+            document.getElementById("cuenta").classList.add("cuenta");
+
+        }
         btnSesion.textContent = "INICIAR SESION";
     }
 }
