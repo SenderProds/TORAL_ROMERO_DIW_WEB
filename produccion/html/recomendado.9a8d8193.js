968cb677ebf4a127c7d75cd804adc989c8dff123
senderprods <alextoralromero04@gmai.com>
2024-03-03 20:03:06 +0100

    Pagina DIW

--- produccion/html/recomendado.9a8d8193.js
+++
@@ -1,8 +0,0 @@
-let videos = document.querySelectorAll(".video");
-videos.forEach((video)=>{
-    video.addEventListener("click", (e)=>{
-        window.open(video.dataset.urlVideo);
-    });
-});
-
-//# sourceMappingURL=recomendado.9a8d8193.js.map
